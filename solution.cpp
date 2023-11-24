97604f95da26dbb2b7c2ed2e941be8b7aab3513d
argArthur <46555016+argArthur@users.noreply.github.com>
2023-11-24 19:34:22 -0300

    ~4.2 seconds

--- solution.cpp
+++ solution.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <bitset>
 
 int size = 5;
 
 int bitCount(unsigned int u)
 {
-     unsigned int uCount;
+    unsigned int uCount;
 
-     uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
-     return ((uCount + (uCount >> 3)) & 030707070707) % 63;
+    uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
+    return ((uCount + (uCount >> 3)) & 030707070707) % 63;
 }
 
 int rowSum(const int state, const int row)
@@ -17,55 +16,69 @@ int rowSum(const int state, const int row)
     return bitCount(selected);
 }
 
-int columnSum(const int state, const int column) 
+int selector;
+int columnSum(const int state, const int column)
 {
-    int m = 1<<(size - 1);
-    int selector= 0;
-    for (int i = 0; i < size; i++)
-        selector = (selector << size) + m;
-    
     const int selected = state & (selector >> column);
     return bitCount(selected);
 }
 
-bool isValidState(const int state, const bool c) 
+int indexOf(int* array, int element)
 {
-    bool hasValues[size*2+1];
-    if (c) for (int i = 0; i < size*2; i++) std::cout<<hasValues[i]<<' ';
-    if (c) std::cout << std::endl;
     for (int i = 0; i < size; i++)
     {
-        if (c) std::cout<< rowSum(state, i) << " r | c " << columnSum(state, i) << '\n';
-        hasValues[rowSum(state, i) - 1] = true;
-        hasValues[columnSum(state, i) + size - 1] = true;
+        if (array[i] == element) return i;
     }
     
-    for (int i = 1; i < size*2; i++)
-        if (hasValues[i] != true) return false;
-    
+    return -1;
+}
+
+bool isValidState(const int state)
+{
+    bool hasValues[size*2];
+    for (int i = 0; i < size*2; i++) hasValues[i] = false;
+    for (int i = 0; i < size; i++)
+    {
+        const int row = rowSum(state, i);
+        if (row == 0) return false; // prevent out of index
+        if (hasValues[row - 1] == true) return false;
+        hasValues[row - 1] = true;
+        
+        const int column = columnSum(state, i);
+        if (column == 0) return false; // prevent out of index
+        if (hasValues[column + size - 1] == true) return false;
+        hasValues[column + size - 1] = true;
+    }
+
     return true;
 }
 
-const int testState = 0b1111111110111001100010000;
+
 int main()
 {
+    clock_t start, end;
+
+    start = clock();
+
+    int m = 1<<(size - 1);
+    selector= 0;
+    for (int i = 0; i < size; i++)
+        selector = (selector << size) + m;
+        
     int count = 0;
     for (int state = 0; state < (1 << (size*size)) - 1; state++)
     {
-        if (isValidState(state, false))
+        if (isValidState(state))
         {
-            bool a = isValidState(state, true);
             count++;
-            std::cout << "\nfound\n\n";
-            std::cout << "is valid: " << a << std::endl;
-            // std::cout << std::bitset<32>(state) << '\n';
-            a = isValidState(state, true);
-            std::cout << "is valid: " << a << std::endl;
-            break;
         }
     }
-    //std::cout << '\n' << isValidState(0b0001000010000100001000011, false) << '\n';
-    std::cout << count << std::endl;
+
+    end = clock();
+    double elapsed = (double(end) - double(start))/CLOCKS_PER_SEC;
     
+    std::cout << count << '\n';
+    std::cout << "time: " << elapsed << " seconds" << std::endl;
+
     return 0;
 }
